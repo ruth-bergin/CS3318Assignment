12ea305bb8153df4d6fb05e9fe17c5b2ef726a58
Ruth Bergin <ruthhelenbergin@gmail.com>
2021-11-25 11:52:12 +0000

    Create test class for AssignmentApplication with a sample test as placeholder.

---
+++ src/main/tests/com/ucc/cs3318assignment/AssignmentApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ucc.cs3318assignment;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AssignmentApplicationTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(4+3, 7);
+    }
+}
