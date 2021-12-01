4f6793bc3c4f7e297d8b2bd0ec82cdf6c0fa8551
Ruth Bergin <ruthhelenbergin@gmail.com>
2021-12-01 11:52:32 +0000

    Begin using regex to check the form of the email address Add check to see if at least one full stop comes after the at symbol

--- src/main/java/com/ucc/cs3318assignment/AssignmentApplication.java
+++ src/main/java/com/ucc/cs3318assignment/AssignmentApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class AssignmentApplication extends Application {
 
     public boolean validateEmailAddress(String emailAddress) {
-        return emailAddress.contains("@") && emailAddress.contains(".");
+        return emailAddress.matches("^\s*@^\s*.^\s*");
     }
 
     @Override
