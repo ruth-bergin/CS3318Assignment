3a72199a9a39816f83711e84dacf91f0464cbf06
Ruth Bergin <ruthhelenbergin@gmail.com>
2021-12-02 12:15:54 +0000

    Create email address and password text fields in GUI

--- src/main/java/com/ucc/cs3318assignment/AssignmentController.java
+++ src/main/java/com/ucc/cs3318assignment/AssignmentController.java
@@ -1,14 +1,30 @@
 package com.ucc.cs3318assignment;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class AssignmentController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label emailLabel;
+
+    @FXML
+    public TextField emailAddress;
+
+    @FXML
+    public Label submitText;
+
+    @FXML
+    public Label passwordLabel;
+
+    @FXML
+    public TextField password;
+
+    @FXML
+    public void onSubmitButtonClick(ActionEvent actionEvent) {
+        emailLabel.setText(emailAddress.getText());
+        passwordLabel.setText(password.getText());
     }
 }
